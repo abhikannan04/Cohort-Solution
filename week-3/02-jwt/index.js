aa8890b8f501ddd961796667a116d8ea0b62319d
Abhilash <abhikannan200304@gmail.com>
2023-12-20 15:14:39 +0530

    Added JWt answer

--- week-3/02-jwt/index.js
+++ week-3/02-jwt/index.js
@@ -1,18 +1,49 @@
 const jwt = require('jsonwebtoken');
 const jwtPassword = "secret";
+const z =  require("zod");
 
+function uservalidaton(username , password){
+    const schema  = z.object({
+        username : z.string().email(),
+        password : z.string().min(6)
+    })
+
+    const userdata =  {
+        username:username,
+        password:password
+    }
+
+    const result = schema.safeParse(userdata);
+
+    return result.success
+}
 function signJwt(username, password) {
-    const token =  jwt.sign({username:username},jwtPassword);
+    if(uservalidaton(username,password)){
+        const token =  jwt.sign({username:username , password:password},jwtPassword);
+        return token;
+    }else{
+        return null;
+    }
     // console.log(token);
 }
 
 function verifyJwt(token) {
-    const verify =  jwt.verify(token,jwtPassword);
+    try{
+        const verify =  jwt.verify(token,jwtPassword);
+        return true;
+    }catch(err){
+        return false;
+    }
     // console.log(verify);
 }
 
 function decodeJwt(token) {
     const decode =  jwt.decode(token);
+    if(decode){
+        return true
+        }else{
+        return false
+        }
     // console.log(decode);
 }
 
